afb2469437a3db48b3ca8779217da859420c1e09
ernesthenry <henry38ernest@gmail.com>
2019-10-08 04:35:42 -0400

    Testing our API

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router = express.Router
+const router = express.Router()
 
 
 // Get all subscribers
@@ -7,26 +7,26 @@ router.get('/', (req, res) => {
     res.send('Welcome Ernest')
 })
 
-//Get one subscriber
-router.get('/:id', (req, res) => {
+// //Get one subscriber
+// router.get('/:id', (req, res) => {
     
-})
+// })
 
-// Create one subscriber
-router.post('/', (req, res) => {
+// // Create one subscriber
+// router.post('/', (req, res) => {
     
-})
+// })
 
-// Update one subscriber
-router.patch('/:id', (req, res) => {
+// // Update one subscriber
+// router.patch('/:id', (req, res) => {
     
-})
+// })
 
 
-// Delete one subscriber
-router.delete('/:id', (req, res) => {
+// // Delete one subscriber
+// router.delete('/:id', (req, res) => {
     
-})
+// })
 
 
 
