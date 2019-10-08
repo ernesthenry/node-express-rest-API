59355e1023686667734ba7c083b1ed437c8d90db
ernesthenry <henry38ernest@gmail.com>
2019-10-08 03:41:56 -0400

    Add routes for add, get, update and delete subscribers

---
+++ routes/subscribers.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router
+
+
+// Get all subscribers
+router.get('/', (req, res) => {
+    res.send('Welcome Ernest')
+})
+
+//Get one subscriber
+router.get('/:id', (req, res) => {
+    
+})
+
+// Create one subscriber
+router.post('/', (req, res) => {
+    
+})
+
+// Update one subscriber
+router.patch('/:id', (req, res) => {
+    
+})
+
+
+// Delete one subscriber
+router.delete('/:id', (req, res) => {
+    
+})
+
+
+
+module.exports = router
